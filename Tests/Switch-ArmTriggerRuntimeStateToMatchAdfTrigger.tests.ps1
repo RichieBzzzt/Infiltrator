f0adb395a62db2f6bc8ce81ca2fdd23e35cb05a8
richiebzzzt <richie@bzzzt.io>
2023-07-24 17:39:25 +0100

    add test

--- Tests/Switch-ArmTriggerRuntimeStateToMatchAdfTrigger.tests.ps1
+++ Tests/Switch-ArmTriggerRuntimeStateToMatchAdfTrigger.tests.ps1
@@ -1,6 +1,33 @@
+BeforeAll {
+    $CommandName = 'Switch-ArmTriggerRuntimeStateToMatchAdfTrigger.ps1'
+    Get-Module Infiltrator | Remove-Module -Force
+    $ModulePath = Join-Path $PSScriptRoot ".."
+    $CommandNamePath = Resolve-Path (Join-Path $ModulePath /Public/$CommandName)
+    Import-Module $CommandNamePath -Force
+}
+
 Describe "Import Module" {
+    BeforeAll{
+     
+    }
     it "Should not throw" {
-        Set-Location $PSScriptRoot
-        Import-Module "..\Infiltrator.psm1" -Force
+        $ResourceGroupName = 'infiltrator'
+        $DataFactoryName = 'infiltrator'
+        $ArmTemplateFile = Join-Path $PSScriptRoot 'arm_templates/test_one/ARMTemplateForFactory.json'
+        { Switch-ArmTriggerRuntimeStateToMatchAdfTrigger `
+            -ResourceGroupName $ResourceGroupName `
+            -DataFactoryName $DataFactoryName `
+            -ArmTemplateFile $ArmTemplateFile } | Should -Not -Throw
+    }
+
+    it "Should not throw whatif" {
+        $ResourceGroupName = 'infiltrator'
+        $DataFactoryName = 'infiltrator'
+        $ArmTemplateFile = Join-Path $PSScriptRoot 'arm_templates/test_one/ARMTemplateForFactory.json'
+        { Switch-ArmTriggerRuntimeStateToMatchAdfTrigger `
+            -ResourceGroupName $ResourceGroupName `
+            -DataFactoryName $DataFactoryName `
+            -ArmTemplateFile $ArmTemplateFile `
+            -WhatIf } | Should -Not -Throw
     }
 }
